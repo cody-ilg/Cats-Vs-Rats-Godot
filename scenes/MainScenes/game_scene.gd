62c12fa2712ab0ca31e8ac4f7b0f619f9a09ac0a
cody-ilg <codyilg@gmail.com>
2025-03-24 17:50:35 +1300

    Changed indentation in game_scene to tabs from spaces to be consistant with the rest of the project. A setting in VScode was converting tabs to spaces

--- scenes/MainScenes/game_scene.gd
+++ scenes/MainScenes/game_scene.gd
@@ -16,91 +16,90 @@ var mobs_in_wave: int = 0
 var player_health: int = 100
 
 func _ready():
-  map_node = get_node("Level1") #turn this into var based on selected map
+	map_node = get_node("Level1") #turn this into var based on selected map
 
-  for i in get_tree().get_nodes_in_group("build_buttons"):
-    i.pressed.connect(initiate_build_mode.bind(i.name))
+	for i in get_tree().get_nodes_in_group("build_buttons"):
+		i.pressed.connect(initiate_build_mode.bind(i.name))
 
 
 func _process(_delta):
-  if build_mode:
-    update_tower_preview()
+	if build_mode:
+		update_tower_preview()
 
 func _unhandled_input(event):
-  if event.is_action_released("ui_cancel") and build_mode == true:
-    cancel_build_mode()
-  if event.is_action_released("ui_accept") and build_mode == true:
-    verify_and_build()
-    cancel_build_mode()
+	if event.is_action_released("ui_cancel") and build_mode == true:
+		cancel_build_mode()
+	if event.is_action_released("ui_accept") and build_mode == true:
+		verify_and_build()
+		cancel_build_mode()
 
 
 ## Building functions
 
 func initiate_build_mode(tower_type):
-  if build_mode:
-    cancel_build_mode()
-  build_type = tower_type + "T1"
-  build_mode = true 
-  get_node("UI").set_tower_preview(build_type, get_global_mouse_position())
+	if build_mode:
+		cancel_build_mode()
+	build_type = tower_type + "T1"
+	build_mode = true
+	get_node("UI").set_tower_preview(build_type, get_global_mouse_position())
 
 func update_tower_preview():
-  var mouse_position = get_global_mouse_position()
-  var current_tile = map_node.get_node("TowerExclusion").local_to_map(mouse_position)
-  var tile_position = map_node.get_node("TowerExclusion").map_to_local(current_tile)
+	var mouse_position = get_global_mouse_position()
+	var current_tile = map_node.get_node("TowerExclusion").local_to_map(mouse_position)
+	var tile_position = map_node.get_node("TowerExclusion").map_to_local(current_tile)
 
-	
-  if map_node.get_node("TowerExclusion").get_cell_source_id(0, current_tile) == -1:
-    get_node("UI").update_tower_preview(tile_position, "4287f5")
-    build_valid = true 
-    build_location = tile_position
-    build_tile = current_tile
-	
-  else:
-    get_node("UI").update_tower_preview(tile_position, "f54242")
-    build_valid = false
+	if map_node.get_node("TowerExclusion").get_cell_source_id(0, current_tile) == -1:
+		get_node("UI").update_tower_preview(tile_position, "4287f5")
+		build_valid = true
+		build_location = tile_position
+		build_tile = current_tile
+
+	else:
+		get_node("UI").update_tower_preview(tile_position, "f54242")
+		build_valid = false
 
 func cancel_build_mode():
-  build_mode = false
-  build_valid = false
-  get_node("UI/TowerPreview").free()
+	build_mode = false
+	build_valid = false
+	get_node("UI/TowerPreview").free()
 
 func verify_and_build():
-  if build_valid:
-    var new_tower = load("res://scenes/towers/" + build_type + ".tscn").instantiate()
-    new_tower.position = build_location
-    new_tower.built = true
-    new_tower.type = str(build_type)
-    new_tower.category = GameData.tower_data[build_type]["category"]
-    map_node.get_node("Turrets").add_child(new_tower, true)
-    map_node.get_node("TowerExclusion").set_cell(0, build_tile, 5, Vector2(1,0))
+	if build_valid:
+		var new_tower = load("res://scenes/towers/" + build_type + ".tscn").instantiate()
+		new_tower.position = build_location
+		new_tower.built = true
+		new_tower.type = str(build_type)
+		new_tower.category = GameData.tower_data[build_type]["category"]
+		map_node.get_node("Turrets").add_child(new_tower, true)
+		map_node.get_node("TowerExclusion").set_cell(0, build_tile, 5, Vector2(1,0))
 
 ## Wave Functions
 
 func start_next_wave():
-  var wave_data = retrieve_wave_data()
-  await(get_tree().create_timer(0.2).timeout)
-  spawn_mobs(wave_data)
+	var wave_data = retrieve_wave_data()
+	await(get_tree().create_timer(0.2).timeout)
+	spawn_mobs(wave_data)
 
 func retrieve_wave_data():
-  var wave_data = [["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ]
-  current_wave += 1
-  mobs_in_wave = wave_data.size()
-  return wave_data
+	var wave_data = [["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ["basic_rat", 0.7], ]
+	current_wave += 1
+	mobs_in_wave = wave_data.size()
+	return wave_data
 
 func spawn_mobs(wave_data):
-  for i in wave_data:
-    var new_mob = load("res://scenes/mobs/" + i[0] + ".tscn").instantiate()
-    # new_mob.connect("reached_end", self, "on_player_damage")
-    map_node.get_node("MobPath").add_child(new_mob, true)
-    await(get_tree().create_timer(i[1]).timeout)
+	for i in wave_data:
+		var new_mob = load("res://scenes/mobs/" + i[0] + ".tscn").instantiate()
+	# new_mob.connect("reached_end", self, "on_player_damage")
+		map_node.get_node("MobPath").add_child(new_mob, true)
+		await(get_tree().create_timer(i[1]).timeout)
 
 ## Damage Functions
 
 func on_player_damage(damage):
-  player_health -= damage
-  if player_health <= 0:
-    print("game over")
-    # emit_signal("game_over", false)
-  else:
-    get_node("UI/HUD/InfoBar/MarginContainer/HBoxContainer/HPBar").update_health_bar(player_health)
-    print(player_health)
+	player_health -= damage
+	if player_health <= 0:
+		print("game over")
+	# emit_signal("game_over", false)
+	else:
+		get_node("UI/HUD/InfoBar/MarginContainer/HBoxContainer/HPBar").update_health_bar(player_health)
+		print(player_health)
